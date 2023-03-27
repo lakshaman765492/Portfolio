ba249722c480623ae7ec04e673e77abbced419a8
lakshaman765492 <105513965+lakshaman765492@users.noreply.github.com>
2023-03-27 19:09:12 +0530

    Personal Portfolio Website

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// -------------Typing Animation ------------
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
